a725949746e2bbd925379236788ac934a1293678
keit1722 <keit1722@gmail.com>
2021-11-03 09:52:06 +0900

    Revert "fizz_buzzメソッドを実装"
    This reverts commit d62979c92ef646a90ba0457f9204cb7cf75ef10e.

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,11 +1,3 @@
 def fizz_buzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    'Fizz Buzz'
-  elsif number % 3 == 0
-    'Fizz'
-  elsif number % 5 == 0
-    'Buzz'
-  else
-    number.to_s
-  end
+  # 実装してください
 end
