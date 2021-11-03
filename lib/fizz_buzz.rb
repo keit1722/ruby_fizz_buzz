f51315ae0b55075fbb5707e333006a06c503d1cb
keit1722 <keit1722@gmail.com>
2021-11-03 12:27:16 +0900

    fizz_buzzメソッドを実装

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,11 @@
 def fizz_buzz(number)
-  # 実装してください
+  if number % 3 == 0 && number % 5 == 0
+    'Fizz Buzz'
+  elsif number % 3 == 0
+    'Fizz'
+  elsif number % 5 == 0
+    'Buzz'
+  else
+    number.to_s
+  end
 end
